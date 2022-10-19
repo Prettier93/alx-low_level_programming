2aecba093d1032bbf4a73f10b6c30335bcbf5249
Prettier93 <prettier.maleka@gmail.com>
2022-10-19 12:15:40 -0700

    error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,12 +2,13 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
+ * Return: returns nothing
  */
 void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
