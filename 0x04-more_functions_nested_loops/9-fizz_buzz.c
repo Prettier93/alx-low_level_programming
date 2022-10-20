dc91c0b5c9209fbed36de137d089291c27705f61
Prettier93 <prettier.maleka@gmail.com>
2022-10-20 13:18:27 -0700

    errors

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,28 @@
+#include "main.h"
 #include <stdio.h>
 /**
- * main - program that prints either number or ..
- * @void: No parameter
+ * main - take the fizz-buzz challenge
  * Return: Return 0 on success
  */
 int main(void)
 {
-	int num,
+	int num;
 
-	while (num++ < 100)
+	for (num = 1; num < 101; num++)
+	{
 
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (num % 5 == 0 && num % 3 == 0)
 			printf("FizzBuzz ");
-		else if ((num % 3) == 0)
-			printf("Fizz ");
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
+		else if (num % 5 == 0)
+			if (num == 100)
 				printf("Buzz ");
 			else
 				printf("Buzz ");
-		}
+		else if (num % 3 == 0)
+			printf("Fizz ");
 		else
 			printf("%d ", num);
+	}
 	printf("\n");
 	return (0);
 }
